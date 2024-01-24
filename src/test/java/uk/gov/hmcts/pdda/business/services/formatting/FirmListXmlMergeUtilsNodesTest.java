b3fdbf188940d295cbe85b161dd1a1794eaac15c
Luke Gittins <luke.gittins@hmcts.net>
2024-01-24 15:54:21 +0000

    PDDA-197 Adding tests to FirmListXmlMergeUtils

---
+++ src/test/java/uk/gov/hmcts/pdda/business/services/formatting/FirmListXmlMergeUtilsNodesTest.java
@@ -0,0 +1,84 @@
+package uk.gov.hmcts.pdda.business.services.formatting;
+
+import org.easymock.EasyMockExtension;
+import org.easymock.TestSubject;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+import uk.gov.hmcts.pdda.web.publicdisplay.rendering.compiled.DocumentUtils;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * <p>
+ * Title: FirmListXMLMergeUtils Second Test Class.
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2024
+ * </p>
+ * <p>
+ * Company: CGI
+ * </p>
+ * 
+ * @author Luke Gittins
+ */
+@ExtendWith(EasyMockExtension.class)
+class FirmListXmlMergeUtilsNodesTest {
+
+    private static final String TRUE = "Result is True";
+    
+    @TestSubject
+    private static FirmListXmlMergeUtils classUnderTest;
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+        classUnderTest = new FirmListXmlMergeUtils();
+    }
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+        // Do nothing
+    }
+
+    @Test
+    void testMergeNode() throws SAXException, IOException, ParserConfigurationException {
+        String documentXml = "<cs:FirmList></cs:FirmList>";
+        String documentXmlToMerge = "<cs:FirmList><cs:ReserveList></cs:ReserveList></cs:FirmList>";
+        boolean result = true;
+        
+        Document baseDocument = getDummyDoc(documentXml);
+        Node node = baseDocument.getDocumentElement();
+        Node nodeToMerge = getDummyDoc(documentXmlToMerge).getDocumentElement();
+        
+        classUnderTest.mergeNode(baseDocument,  node, nodeToMerge);
+        assertTrue(result, TRUE);
+    }
+    
+    @Test
+    void testMergeNodeReserved() throws SAXException, IOException, ParserConfigurationException {
+        String documentXml = "<cs:FirmList></cs:FirmList>";
+        String documentXmlToMerge = "<cs:ReserveList></cs:ReserveList>";
+        boolean result = true;
+        
+        Document baseDocument = getDummyDoc(documentXml);
+        Node node = baseDocument.getDocumentElement();
+        Node nodeToMerge = getDummyDoc(documentXmlToMerge).getDocumentElement();
+        
+        classUnderTest.mergeNode(baseDocument,  node, nodeToMerge);
+        assertTrue(result, TRUE);
+    }
+    
+    private Document getDummyDoc(String xml) throws SAXException, IOException, ParserConfigurationException {
+        return DocumentUtils.createInputDocument(xml);
+    }
+}
