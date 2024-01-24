d3cdc2fde3b6495ae65224fd579613040a983e79
Luke Gittins <luke.gittins@hmcts.net>
2024-01-24 13:23:48 +0000

    PDDA-197 Increasing coverage on CompareXmlNodesTest

--- src/test/java/uk/gov/hmcts/pdda/business/services/formatting/CompareXmlNodesTest.java
+++ src/test/java/uk/gov/hmcts/pdda/business/services/formatting/CompareXmlNodesTest.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import uk.gov.hmcts.framework.services.CsServices;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * <p>
@@ -30,9 +35,13 @@ class CompareXmlNodesTest {
     private static final int EQUAL_TO = 0;
 
     private static final String EQUAL = "Result is Equal";
+    private static final String TRUE = "Result is True";
+    private static final String FALSE = "Result is False";
     
     private static final String NODENAME = "NodeName";
     private static final String COURT1 = "Court 1";
+    
+    private static final String TEST_NODE_BLANK_XML = "<courtroomname></courtroomname>";
 
     @BeforeAll
     public static void setUp() throws Exception {
@@ -45,61 +54,52 @@ class CompareXmlNodesTest {
     }
 
     @Test
-    void testCompareCourtHouseNodesReplacePrefix() {
+    void testCompareCourtHouseNodes() {
         assertEquals(EQUAL_TO,
             CompareXmlNodes.compareCourtHouseNodes("at TestValue", "at TestValue"), EQUAL);
-    }
-
-    @Test
-    void testCompareCourtHouseNodesNoReplace() {
         assertEquals(EQUAL_TO, CompareXmlNodes.compareCourtHouseNodes("TestValue", "TestValue"),
             EQUAL);
     }
 
     @Test
-    void testCompareNodesIwpCourtNumeric() {
+    void testCompareNodesIwp() {
         assertEquals(EQUAL_TO,
             CompareXmlNodes.compareNodes(NODENAME, COURT1, COURT1, true, false), EQUAL);
-    }
-
-    @Test
-    void testCompareNodesIwpCourtNonNumeric() {
         assertEquals(EQUAL_TO,
             CompareXmlNodes.compareNodes(NODENAME, "Court Test", "Court Test", true, false),
             EQUAL);
-    }
-
-    @Test
-    void testCompareNodesIwpCrownCourt() {
         assertEquals(EQUAL_TO,
             CompareXmlNodes.compareNodes(NODENAME, "Crown Court 1", "Crown Court 1", true, false),
             EQUAL);
-    }
-
-    @Test
-    void testCompareNodesIwpCrownCourtNonNumeric() {
         assertEquals(EQUAL_TO, CompareXmlNodes.compareNodes(NODENAME, "Crown Court Test",
             "Crown Court Test", true, false), EQUAL);
-    }
-
-    @Test
-    void testCompareNodesIwpNoCourt() {
         assertEquals(EQUAL_TO,
             CompareXmlNodes.compareNodes(NODENAME, "Test", "Test", true, false), EQUAL);
     }
 
     @Test
-    void testCompareNodesWarnedList() {
+    void testCompareNodes() {
         assertEquals(EQUAL_TO,
             CompareXmlNodes.compareNodes("cs:CourtHouseName", COURT1, COURT1, false, true),
             EQUAL);
-    }
-
-    @Test
-    void testCompareNodesFalseOutcome() {
         assertEquals(EQUAL_TO,
             CompareXmlNodes.compareNodes("cs:CourtHouseName", COURT1, COURT1, false, false),
             EQUAL);
     }
-
+    
+    @Test
+    void testCompareToEmpty() {
+        assertTrue(CompareXmlNodes.compareToEmpty(NODENAME, ""), TRUE);
+        assertFalse(CompareXmlNodes.compareToEmpty(NODENAME, NODENAME), FALSE);
+    }
+    
+    @Test
+    void testIsBlankTextNode() {
+        assertFalse(CompareXmlNodes.isBlankTextNode(getDummyNode(TEST_NODE_BLANK_XML)), FALSE);
+    }
+    
+    private Node getDummyNode(String xml) {
+        Document doc = CsServices.getXmlServices().createDocFromString(xml);
+        return doc.getDocumentElement();
+    }
 }
