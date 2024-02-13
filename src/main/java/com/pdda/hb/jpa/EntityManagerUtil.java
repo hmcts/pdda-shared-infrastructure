5cd5302a455237278ef7f9c60ae930ac3dac76a6
Luke Gittins <luke.gittins@hmcts.net>
2024-02-13 11:30:29 +0000

    PDDA-197 Adding back the log lines for EntityManagerUtil and PddaEntityHelper

--- src/main/java/com/pdda/hb/jpa/EntityManagerUtil.java
+++ src/main/java/com/pdda/hb/jpa/EntityManagerUtil.java
@@ -4,8 +4,11 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.FlushModeType;
 import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EntityManagerUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(EntityManagerUtil.class);
     private static final EntityManagerFactory ENTITYMANAGERFACTORY;
     
     static {
@@ -13,6 +16,7 @@ public class EntityManagerUtil {
             ENTITYMANAGERFACTORY = Persistence.createEntityManagerFactory("PDDA");
 
         } catch (RuntimeException ex) {
+            LOG.error("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
