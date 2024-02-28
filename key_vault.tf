40eb7a1a54c421bdfab32f0322d350a95956c457
harleyfinleycgi <harley.finley@hmcts.net>
2024-02-28 08:59:23 +0000

    destroy dev

--- key_vault.tf
+++ key_vault.tf
@@ -1,13 +1,13 @@
-module "pdda_key_vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-
-  name                    = "${var.product}-${var.env}"
-  product                 = var.product
-  env                     = var.env
-  object_id               = var.jenkins_AAD_objectId
-  resource_group_name     = azurerm_resource_group.pdda_resource_group.name
-  product_group_name      = "DTS SDS PDDA"
-  create_managed_identity = true
-
-  common_tags = var.common_tags
-}
+#module "pdda_key_vault" {
+#  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+#
+#  name                    = "${var.product}-${var.env}"
+#  product                 = var.product
+#  env                     = var.env
+#  object_id               = var.jenkins_AAD_objectId
+#  resource_group_name     = azurerm_resource_group.pdda_resource_group.name
+#  product_group_name      = "DTS SDS PDDA"
+#  create_managed_identity = true
+#
+#  common_tags = var.common_tags
+#}
