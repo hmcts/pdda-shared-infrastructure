6d3d962ee2159cc426eed03909871bf1374d8727
Harley Finley <harley.finley@hmcts.net>
2024-05-13 13:05:08 +0100

    pdm app insights

--- app-insights.tf
+++ app-insights.tf
@@ -43,14 +43,14 @@ moved {
   from = azurerm_application_insights.appinsights
   to   = module.application_insights.azurerm_application_insights.this
 }
-resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "app-insights-connection-string"
+resource "azurerm_key_vault_secret" "app_insights_connection_string_pdm" {
+  name         = "app-insights-connection-string-pdm"
   value        = module.application_insights.connection_string
   key_vault_id = module.pdda_key_vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "azure_appinsights_key" {
-  name         = "AppInsightsInstrumentationKey"
+resource "azurerm_key_vault_secret" "azure_appinsights_key_pdm" {
+  name         = "AppInsightsInstrumentationKey-pdm"
   value        = module.application_insights.instrumentation_key
   key_vault_id = module.pdda_key_vault.key_vault_id
 }
