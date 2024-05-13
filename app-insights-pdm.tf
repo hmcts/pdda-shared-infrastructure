1b9dea887bdd542b919d2e90d4bae9c0b955d33d
Harley Finley <harley.finley@hmcts.net>
2024-05-13 13:19:36 +0100

    pdm app insights

--- app-insights-pdm.tf
+++ app-insights-pdm.tf
@@ -1,4 +1,4 @@
-module "application_insights" {
+module "application_insights_pdm" {
   source   = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
   location = azurerm_resource_group.pdda_resource_group.location
   env      = var.env
@@ -11,8 +11,8 @@ module "application_insights" {
 }
 
 moved {
-  from = azurerm_application_insights.appinsights
-  to   = module.application_insights.azurerm_application_insights.this
+  from = azurerm_application_insights_pdm.appinsights
+  to   = module.application_insights_pdm.azurerm_application_insights.this
 }
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string_pdm" {
