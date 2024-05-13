45f0f1405a5dfc8168f78009af9d2ab708b9020e
Harley Finley <harley.finley@hmcts.net>
2024-05-13 13:08:12 +0100

    pdm app insights

--- app-insights.tf
+++ app-insights.tf
@@ -2,7 +2,7 @@ module "application_insights" {
   source   = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
   location = azurerm_resource_group.pdda_resource_group.location
   env      = var.env
-  product  = var.product
+  product  = [var.product, "pdm"]
 
 
   resource_group_name = azurerm_resource_group.pdda_resource_group.name
@@ -26,23 +26,6 @@ resource "azurerm_key_vault_secret" "azure_appinsights_key" {
   key_vault_id = module.pdda_key_vault.key_vault_id
 }
 
-
-module "application_insights" {
-  source   = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
-  location = azurerm_resource_group.pdda_resource_group.location
-  env      = var.env
-  product  = "pdm"
-
-
-  resource_group_name = azurerm_resource_group.pdda_resource_group.name
-
-  common_tags = var.common_tags
-}
-
-moved {
-  from = azurerm_application_insights.appinsights
-  to   = module.application_insights.azurerm_application_insights.this
-}
 resource "azurerm_key_vault_secret" "app_insights_connection_string_pdm" {
   name         = "app-insights-connection-string-pdm"
   value        = module.application_insights.connection_string
