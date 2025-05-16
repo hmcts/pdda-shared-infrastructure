f3bbc4c42fd574128c1f585408fe1086f75dd88a
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2025-05-16 15:43:54 +0000

    Updating Terraform Formatting

--- app-insights.tf
+++ app-insights.tf
@@ -1,13 +1,13 @@
 module "application_insights" {
   source   = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
   location = azurerm_resource_group.pdda_resource_group.location
-  
-  env      = var.env
-  product  = var.product
+
+  env     = var.env
+  product = var.product
 
   resource_group_name = azurerm_resource_group.pdda_resource_group.name
 
-  common_tags = var.common_tags
+  common_tags         = var.common_tags
   alert_limit_reached = true
 }
 
