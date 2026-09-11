607a7239eec3a5173eb52639a0b93ff561130d69
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-11 12:24:33 +0100

    add data resource

--- main.tf
+++ main.tf
@@ -8,3 +8,8 @@ provider "azurerm" {
   alias                           = "postgres_network"
   subscription_id                 = var.aks_subscription_id
 }
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env == "sandbox" ? "sbox" : var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
